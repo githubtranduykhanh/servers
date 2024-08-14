d4e19c01a7da3b0c8bf25936ed4c155ee6acfd14
githubtranduykhanh <tranduykhanh.210101@gmail.com>
2024-08-15 01:32:28 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
+
+const app = express()
+
+const initRoutes = require('./src/routers')
+
+console.log(process.env.URL_CLIENT)
+
+app.use(cors({
+    origin:process.env.URL_CLIENT,
+    credentials:true //Lưu cookie trên trình duyệt cùng với cấu hình withCredentials:true axios
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+app.get("/", (req,res) => {  
+    res.json({
+        status:true,
+        data:[]
+    })
+})
+
+initRoutes(app)
+
+const port = process.env.PORT || 8888
+
+app.listen(port,()=>{console.log('Server is runing on the port '+ port)})
